d8a3e5b80bb8d96a1909090e933ded654697583d
Achmad Rizky Syahrani <riskiamad@gmail.com>
2022-08-09 16:07:52 +0700

    add client for role

--- web/handler/role.go
+++ web/handler/role.go
@@ -1,9 +1,41 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"task-scheduler/config"
+
+	"github.com/gin-gonic/gin"
+)
 
 type RoleHandler struct{}
 
 func (h *RoleHandler) URLMapping(r *gin.RouterGroup) {
+	r.GET("", h.index)
+	r.GET("/:id", h.detail)
+}
 
+func (h *RoleHandler) index(c *gin.Context) {
+	roles, err := http.Get(config.Config.APIBaseURL + "/role")
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
+	}
+
+	defer roles.Body.Close()
+
+	c.HTML(http.StatusOK, "role_index.html", gin.H{"roles": roles})
+}
+
+func (h *RoleHandler) detail(c *gin.Context) {
+	id := c.Param("id")
+
+	role, err := http.Get(config.Config.APIBaseURL + "/role/" + id)
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
+	}
+
+	defer role.Body.Close()
+
+	c.HTML(http.StatusOK, "role_index.html", gin.H{"role": role})
 }
